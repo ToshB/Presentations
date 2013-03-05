e7b74fd771046a34ba3417564e706868c1c9e96b
Torstein Bjørnstad <torstein.bjornstad@gyldendal.no>
2013-03-05 22:12:07 +0100

    Add JavaScript Testing Project Example

---
+++ JavaScriptTesting/Example/Algorithms.Tests/CalculatorTests.Jasmine.js
@@ -0,0 +1,25 @@
+﻿/// <reference path="../Algorithms/Calculator.js" />
+
+describe("Calculator", function () {
+    var calc = new Calculator();
+
+    it("returns a number when called with positive number", function () {
+        var num = calc.factorial(4);
+        
+        expect(num).toBeGreaterThan(0);
+    });
+
+    it("should throw when given a negative number", function () {
+        var delegate = function () {
+            calc.factorial(-1);
+        };
+
+        expect(delegate).toThrow();
+    });
+
+    it("should give expected value for input 10", function () {
+        var num = calc.factorial(10);
+
+        expect(num).toEqual(3628800);
+    });
+});
---
+++ JavaScriptTesting/Example/Algorithms.Tests/CalculatorTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace Algorithms.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator _calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Test]
+        public void Factorial_WithPositiveNumber_Completes()
+        {
+            var num = _calculator.Factorial(4);
+
+            Assert.That(num, Is.GreaterThan(0));
+        }
+
+        [Test]
+        public void Factorial_WithNegativeNumber_Throws()
+        {
+            TestDelegate testDelegate = () => _calculator.Factorial(-1);
+            Assert.That(testDelegate, Throws.ArgumentException);
+        }
+
+        [Test]
+        public void Factorial_With10_ReturnsExpectedValue()
+        {
+            var num = _calculator.Factorial(10);
+
+            Assert.That(num, Is.EqualTo(3628800));
+        }
+    }
+}
